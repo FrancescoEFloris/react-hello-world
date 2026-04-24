c4d8995fc491b486df6e8ebfcd2e8d070fc57597
Francesco Floris <francescofloris.ef@gmail.com>
2026-04-24 14:48:47 +0200

    try 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
 
 function App() {
-  return (
-  <h1>Hello World!</h1>
-  );
+  let helloContainer = [];
+  for (let i = 0; i < 5; i++) {
+    helloContainer.push( <>
+      <h1>{1 + i} Hello World!</h1>
+      <p>hello human</p>
+    </>);
+  }
+  return helloContainer;
 }
 
 export default App
